c7e3e486453e14ac72aff2bcd7441425307eb673
Peter Davies <ultratwo@gmail.com>
2022-01-13 17:36:21 +0000

    Implement EIP 170

--- src/ethereum/spurious_dragon/vm/interpreter.py
+++ src/ethereum/spurious_dragon/vm/interpreter.py
@@ -14,7 +14,7 @@ A straightforward interpreter that executes EVM code.
 from typing import Set, Tuple, Union
 
 from ethereum.base_types import U256, Bytes0, Uint
-from ethereum.utils.ensure import EnsureError
+from ethereum.utils.ensure import EnsureError, ensure
 
 from ..eth_types import Address, Log
 from ..state import (
@@ -26,6 +26,7 @@ from ..state import (
     rollback_transaction,
     set_code,
     touch_account,
+    increment_nonce,
 )
 from ..vm import Message
 from ..vm.error import (
@@ -44,6 +45,7 @@ from .instructions import Ops, op_implementation
 from .runtime import get_valid_jump_destinations
 
 STACK_DEPTH_LIMIT = U256(1024)
+MAX_CODE_SIZE = 0x6000
 
 
 def process_message_call(
@@ -113,12 +115,14 @@ def process_create_message(message: Message, env: Environment) -> Evm:
     # take snapshot of state before processing the message
     begin_transaction(env.state)
 
+    increment_nonce(env.state, message.current_target)
     evm = process_message(message, env)
     if not evm.has_erred:
         contract_code = evm.output
         contract_code_gas = len(contract_code) * GAS_CODE_DEPOSIT
         try:
             evm.gas_left = subtract_gas(evm.gas_left, contract_code_gas)
+            ensure(len(contract_code) <= MAX_CODE_SIZE, OutOfGasError)
         except OutOfGasError:
             rollback_transaction(env.state)
             evm.gas_left = U256(0)
